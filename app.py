be3a05d1054adf75761c0b85910797a8e1b65570
fianngr <alfiannugroho95@gmail.com>
2023-12-22 13:43:51 +0700

    change file

--- app.py
+++ app.py
@@ -131,13 +131,13 @@ def login ():
         # Check if the user exists
         if user:
             print(user)
-            print(user[2])
-            hashed_password = user[3]
+            print(user[0]['username'])
+            hashed_password = user[0]['password']
             # print(user[3])
             if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
                 # print(user[3])
                     # Jika password valid, buat token JWT
-                token = create_access_token(identity={'username': user[2]})
+                token = create_access_token(identity={'username': user[0]['username']})
                 return jsonify({
                     'message': 'Login Success',
                     'token_jwt': token
